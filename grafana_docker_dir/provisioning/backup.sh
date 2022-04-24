65da719e09346780693313709cb3e0a1c66056c5
muralisc <muralisc@gmail.com>
2022-04-24 20:12:24 +0100

    Remove data sources

--- grafana_docker_dir/provisioning/backup.sh
+++ grafana_docker_dir/provisioning/backup.sh
@@ -1,8 +1,8 @@
-YOUR_GRAFANA_URL="http://192.168.0.237:3000"
+YOUR_GRAFANA_URL="http://mirkwood:3000"
 YOUR_GRAFANA_ADMIN_ACCOUNT=admin
 YOUR_GRAFANA_ADMIN_PASSWORD=adminp
 YOUR_BACKUP_FOLDER_ON_THE_HOST=$PWD
-YOUR_GRAFANA_TOKEN=eyJrIjoiNGpnaDRvWHlDZDhtMkpCemRla2ZyQ3JXOWlRMG9lc2wiLCJuIjoiYmFja3VwIiwiaWQiOjF9
+YOUR_GRAFANA_TOKEN="eyJrIjoiaU9OSzU3M0xwTjcwdWF5OVBHRVR0U2hqSTZMajUwY2oiLCJuIjoiYXNkZiIsImlkIjoxfQ=="
 
 docker run --user $(id -u):$(id -g) --rm --name grafana-backup-tool \
            -e GRAFANA_TOKEN=${YOUR_GRAFANA_TOKEN} \
